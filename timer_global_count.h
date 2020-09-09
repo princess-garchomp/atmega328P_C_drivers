2cc937c5ffc39f1b1db56cec1fa3a7e42dc82961
princess-garchomp <67834462+princess-garchomp@users.noreply.github.com>
2020-09-09 16:40:22 -0400

    Add files via upload

---
+++ timer_global_count.h
@@ -0,0 +1,14 @@
+#ifndef TIEMR_GLOBAL_COUNT_H
+#define TIEMR_GLOBAL_COUNT_H
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void _16_bit_tiemr1_config();
+
+
+//.1ms
+unsigned long _mS_counter();
+
+#endif
