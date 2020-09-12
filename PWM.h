9883392c45a080c9a52648184882dfb875179f6b
princess-garchomp <67834462+princess-garchomp@users.noreply.github.com>
2020-09-12 11:56:42 -0400

    Update PWM.h

--- PWM.h
+++ PWM.h
@@ -1,8 +1,14 @@
-#IFNDEF PWM_H
-#DEFINE PWM_H
+#ifndef PWM_H
+#define PWM_H
+
+#include <stdint.h>
 void _16_bit_tiemr1_config();
 void tiemr_1_OCRA1_set(uint16_t data);
 void tiemr_1_OCRB1_set(uint16_t data);
 
+uint16_t timer_1_get_OCRA1();
+uint16_t timer_1_get_OCRB1();
+void milliS_timer_config(uint8_t milliS);
 
-#ENDIF
+
+#endif
