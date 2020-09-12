848776e25a0dc87f9ac0b94a72e2e112cf7928b4
princess-garchomp <67834462+princess-garchomp@users.noreply.github.com>
2020-09-12 10:50:55 -0400

    Update PWM.c

--- PWM.c
+++ PWM.c
@@ -1,15 +1,20 @@
 #include "PWM.h"
 
-void _16_bit_tiemr1_config(int frequency){
 
-		//cs12 is prescaler of 254
+
+void _16_bit_tiemr1_config(){
+
+		//no prescaler
     //wgm12 is CTC mode
-	 TCCR1B |= (1ul<<(CS12)) | (1ul<<(WGM12));
+	 TCCR1B |= (1ul<<(CS10)) | (1ul<<(WGM12));
    //overfolw interrupt enable
-	 TIMSK1 |= (1ul<<(OCIE1A));
+	 TIMSK1 |= (1ul<<(OCIE1A)|(1ul<<OCIE1B));
 	 //OCRnA=F_CPU/((frequency*2*N)-1)
-	 OCR1A = (int)frequency;
+	 OCR1A = 800;
+	OCR1B = 100;
 	 //here we set the frequency by changing the value of N
 }
 
-//ISRTIMER1_COMPA_vect){}
+//ISRTIMER1_COMPA_vect){on led;}
+
+//ISR(TIMER1_COMPB_vect){led off;}
